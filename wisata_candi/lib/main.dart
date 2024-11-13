579390062c5a927dd51a2534fce7aa1c43e71cbf
Djali Suandre <djalisuandre_2226240011@mhs.mdp.ac.id>
2024-11-13 10:27:28 +0700

    updates part 3

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
